08d53e7813b9186de0b84130c8b6b53e7afcc5b6
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2022-01-29 12:49:24 -0300

    Menu Principal e Sistema de áudio

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject MainMenuUI;
+    public GameObject SettingsUI;
+    public GameObject CreditsUI;
+
+    public void LoadPlay()
+    {
+        //AudioManager.instance.Play("ButtonClick");
+        SceneManager.LoadScene("Play");
+
+    }
+
+    public void LoadSettings()
+    {
+        //AudioManager.instance.Play("ButtonClick");
+        SettingsUI.SetActive(true);
+    }
+    public void QuitSettings()
+    {
+        //AudioManager.instance.Play("ButtonClick");
+        SettingsUI.SetActive(false);
+    }
+
+    public void LoadCredits()
+    {
+        //AudioManager.instance.Play("ButtonClick");
+        MainMenuUI.SetActive(false);
+        CreditsUI.SetActive(true);
+    }
+
+    public void QuitCredits()
+    {
+        //AudioManager.instance.Play("ButtonClick");
+        CreditsUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+    }
+
+}
