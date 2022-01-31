dd73e1166389103197ffc20685d9ca3ce74d51aa
vitorsalek <59521603+vitorsalek@users.noreply.github.com>
2022-01-31 02:27:58 -0300

    fix botoes, fix no settings e créditos finais com tempo final

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,21 +6,28 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public bool isFinalScene;
     public Text timerText;
     [HideInInspector] public float timer;
 
     private void Start()
     {
         timer = Persistent.current.timer;
+        TimeSpan TimeSpan = TimeSpan.FromSeconds(timer);
+        string output = String.Format("{0}:{1}", TimeSpan.Minutes.ToString("00"), TimeSpan.Seconds.ToString("00"));
+        timerText.text = output;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        TimeSpan TimeSpan = TimeSpan.FromSeconds(timer);
-        string output = String.Format("{0}:{1}", TimeSpan.Minutes.ToString("00"), TimeSpan.Seconds.ToString("00"));
-        timerText.text = output;
+        if (!isFinalScene)
+        {
+            timer += Time.deltaTime;
+            TimeSpan TimeSpan = TimeSpan.FromSeconds(timer);
+            string output = String.Format("{0}:{1}", TimeSpan.Minutes.ToString("00"), TimeSpan.Seconds.ToString("00"));
+            timerText.text = output;
+        }
     }
 
     private void OnDestroy()
